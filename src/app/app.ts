2b0595921ae6f51b78706f60a3c621f7f362c95a
@rizwan.ullah <rizwan.ullah@plcgroup.com>
2025-10-08 12:25:11 +0500

    react to angular switching

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,8 +4,10 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  // templateUrl: './app.html',
+  template: `<h1>Hello World</h1>`,
+  styles: `h1{ color: red; }`,
+  // styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('react-to-angular');
