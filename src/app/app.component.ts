8812b03886da330efc740937193eaafa41f28f85
rizwan Ullah <rizwan.signon@gmail.com>
2025-10-12 12:59:37 +0500

    add necessary components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component, effect, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Home } from './home/home';
-import { Header } from './components/header/header';
-import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Home, Header, Footer],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
